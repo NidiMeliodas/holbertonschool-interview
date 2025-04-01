b45f33e78fb4c7bb761d476f711b4d382499b993
NidiMeliodas <nidigashan@gmail.com>
2025-04-01 10:58:23 +0200

    test

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,25 +1,29 @@
-#!/usr/bin/python3
 """
-Exercice :
+Exercice:
 You have n number of locked boxes in front of you.
 Each box is numbered sequentially from 0 to n - 1
 and each box may contain keys to the other boxes.
 Write a method that determines if all the boxes can be opened.
 """
 
-def can_unlock_all(boxes):
+def canUnlockAll(boxes):
     """
-    This function wil check if all the boxes can be opened using
-    a key found in another box
+    This function will check if all the boxes can be opened using
+    a key found in another box.
     """
+    if not isinstance(boxes, list):
+        return False
+    if len(boxes) == 0:
+        return False
+    
     n = len(boxes)
     opened = {0}  # Start with the first box opened
     keys = set(boxes[0])  # Start with keys from the first box
-
+    
     while keys:
-        key = keys.pop()  # Take a key
-        if key < n and key not in opened:  # Check if it unlocks a new box
-            opened.add(key)  # Mark box as opened
-            keys.update(boxes[key])  # Add new keys from this box
-
-    return len(opened) == n  # True if all boxes are opened
+        key = keys.pop()
+        if 0 <= key < n and key not in opened:
+            opened.add(key)
+            keys.update(boxes[key])
+    
+    return len(opened) == n
