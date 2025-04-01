011339104a25f8a1290219ccaf6cee7e85a888c5
NidiMeliodas <nidigashan@gmail.com>
2025-04-01 09:55:41 +0200

    test

--- lockboxes/main_0.py
+++ lockboxes/main_0.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-
+"""
+    Determines if all boxes can be opened.
+    
+    Args:
+        boxes (list of list of int): A list where each index represents a box, 
+        and the list at each index contains keys to other boxes.
+    
+    Returns:
+        bool: True if all boxes can be opened, else False.
+    """
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
 boxes = [[1], [2], [3], [4], []]
