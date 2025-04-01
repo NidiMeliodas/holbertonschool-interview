07522963833f641f9debecc856ff7b8892f22dfe
NidiMeliodas <nidigashan@gmail.com>
2025-04-01 11:05:15 +0200

    pycodestyle

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -7,18 +7,19 @@ and each box may contain keys to the other boxes.
 Write a method that determines if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
     """
     This function will check if all the boxes can be opened using
     a key found in another box.
-    
+
     Args:
         boxes (list of lists): A list where each index represents a box,
         and the inner list contains keys to other boxes.
-    
+
     Returns:
         bool: True if all boxes can be opened, otherwise False.
-    
+
     Explanation:
         - We start with the first box (index 0) already opened.
         - We collect all keys found in the first box.
@@ -31,15 +32,17 @@ def canUnlockAll(boxes):
         return False
     if len(boxes) == 0:  # Edge case: empty list
         return False
-    
+
     n = len(boxes)
     opened = {0}  # Start with the first box opened
     keys = set(boxes[0])  # Collect keys from the first box
-    
+
     while keys:  # While there are keys to process
         key = keys.pop()  # Take a key from the set
-        if 0 <= key < n and key not in opened:  # Check if key unlocks a new box
+        # Check if key unlocks a new box
+        if 0 <= key < n and key not in opened:
             opened.add(key)  # Mark box as opened
-            keys.update(boxes[key])  # Add new keys from this box to our collection
-    
+            # Add new keys from this box to our collection
+            keys.update(boxes[key])
+
     return len(opened) == n  # Return True if all boxes are opened
